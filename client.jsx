004c1d7478e06b27821022217477a9201721b7d0
jaehoon0822 <jaehoon0822@gmail.com>
2019-08-19 20:10:42 +0900

    wwebpac

--- client.jsx
+++ client.jsx
@@ -1,5 +1,8 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+require('object-assign');
+require('prop-types');
+
 const WordRelay = require('./WordRelay');
 
 ReactDOM.render(<WordRelay />, document.querySelector('#root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,32 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {	
-	mode: 'development',
-	devtool: 'eval',
+module.exports = {
+	name: 'wordrelay-setting',
+	mode: 'development', // 실서비스: production
+	devtool: 'eval', // hidden-source-map
 	resolve: {
-		extensions: ['.jsx', '.js'],
+		extensions: ['.jsx', 'js', '.ts'],
 	},
-
 	entry: {
-		app: './client'	
+		app: ['./client.jsx'],
 	},
-
 	module: {
 		rules: [{
 			test: /\.jsx?$/,
 			loader: 'babel-loader',
-		options: {	
-		presets: [['@babel/preset-env', {
-				targets: {
-					browsers: ['> 1% in KR'],
-				},
-				debug: true,	
-			}], '@babel/preset-react'],
-				plugins: ['@babel/plugin-proposal-class-properties']
+			options: {
+				presets: [ '@babel/preset-env', '@babel/preset-react' ],
+				plugins: ['@babel/plugin-proposal-class-properties'],
 			},
+			exclude: /node_modules/,
 		}],
 	},
-
 	plugins: [
-		new webpack.LoaderOptionsPlugin({ debug: true }),
-	],
 
+	],
 	output: {
+		path: path.join(__dirname, 'dist'),
 		filename: 'app.js',
-		path: path.join(__dirname, 'dist')
-	},
+	}
 };
