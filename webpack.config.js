73a99b86cf900a774001b5d913bff58ca4389f46
jaehoon0822 <jaehoon0822@gmail.com>
2019-08-19 21:57:44 +0900

    error is what?

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,108 @@
 const path = require('path');
 const webpack = require('webpack');
 
+// module.exports = {
+// 	mode: 'development', // 실서비스: production
+// 	devtool: 'eval', // hidden-source-map
+// 	resolve: {
+// 		extensions: ['.jsx', 'js', '.ts'],
+// 	},
+// 	entry: {
+// 		app: ['./client.jsx'],
+// 	},
+// 	module: {
+// 		rules: [{
+// 			test: /\.jsx?$/,
+// 			loader: 'babel-loader',
+// 			options: {
+// 				presets: [ ['@babel/preset-env', {
+// 					targets: {
+// 						browsers: ['> 1% in KR'],
+// 					}
+// 				}], '@babel/preset-react' ],
+// 				plugins: ['@babel/plugin-proposal-class-properties'],
+// 			},
+// 		}],
+// 	},
+// 	plugins: [
+// 		new webpack.LoaderOptionsPlugin({ debug: true }),
+// 	],
+// 	output: {
+// 		path: path.join(__dirname, 'dist'),
+// 		filename: 'app.js',
+// 	}
+// };
+
 module.exports = {
-	name: 'wordrelay-setting',
 	mode: 'development', // 실서비스: production
 	devtool: 'eval', // hidden-source-map
 	resolve: {
-		extensions: ['.jsx', 'js', '.ts'],
+		extensions: ['.jsx', 'js'],
 	},
+	
 	entry: {
-		app: ['./client.jsx'],
+		app: './client',
 	},
+
 	module: {
 		rules: [{
 			test: /\.jsx?$/,
 			loader: 'babel-loader',
-			options: {
-				presets: [ '@babel/preset-env', '@babel/preset-react' ],
-				plugins: ['@babel/plugin-proposal-class-properties'],
+			options: {	
+				presets: [['@babel/preset-env', {
+						targets: {
+							browsers: ['> 1% in KR'],
+						},
+						debug: true,	
+					}], '@babel/preset-react'],
+				plugins: ['@babel/plugin-proposal-class-properties']
 			},
-			exclude: /node_modules/,
 		}],
 	},
-	plugins: [
 
-	],
+ 	plugins: [
+ 		new webpack.LoaderOptionsPlugin({ debug: true }),
+ 	],
+
 	output: {
-		path: path.join(__dirname, 'dist'),
 		filename: 'app.js',
+		path: path.join(__dirname, 'dist'),
 	}
 };
+
+// module.exports = {	
+// 	mode: 'development',
+// 	devtool: 'eval',
+// 	resolve: {
+// 		extensions: ['.jsx', '.js'],
+// 	},
+
+// 	entry: {
+// 		app: './client'	
+// 	},
+
+// 	module: {
+// 		rules: [{
+// 			test: /\.jsx?$/,
+// 			loader: 'babel-loader',
+// 			options: {	
+// 				presets: [['@babel/preset-env', {
+// 						targets: {
+// 							browsers: ['> 1% in KR'],
+// 						},
+// 						debug: true,	
+// 					}], '@babel/preset-react'],
+// 				plugins: ['@babel/plugin-proposal-class-properties']
+// 			},
+// 		}],
+// 	},
+
+// 	plugins: [
+// 		new webpack.LoaderOptionsPlugin({ debug: true }),
+// 	],
+
+// 	output: {
+// 		filename: 'app.js',
+// 		path: path.join(__dirname, 'dist')
+// 	},
+// };
